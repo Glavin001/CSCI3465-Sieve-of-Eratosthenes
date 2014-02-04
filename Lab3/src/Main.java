0c36e01a8809fc90959e06eba236e97c91f8ec68
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-02-04 19:30:32 -0400

    First commit.

---
+++ Lab3/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author glavin
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
